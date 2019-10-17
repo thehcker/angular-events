9f7cff0b54d492f1a90c9f7bc5d8ec364582e16a
ISAAC NYAKOI <isaacnyakoi1@gmail.com>
2019-10-17 23:46:58 +0300

    Add Angular app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.component';
+import { EventsThumbnailComponent } from './events/events-thumbnail.component';
+import { NavBarComponent } from './nav/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+
+  declarations: [
+    EventsAppComponent,
+    EventsListComponent,
+    EventsThumbnailComponent,
+    NavBarComponent
+  ],
+  bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
