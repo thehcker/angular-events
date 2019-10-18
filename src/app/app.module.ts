31c920e29034a3dd4cf79cc64394a053ef3f99e8
ISAAC NYAKOI <isaacnyakoi1@gmail.com>
2019-10-18 03:29:15 +0300

    Create templates and Re-usable angular services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventsThumbnailComponent } from './events/events-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
+import { EventService } from './events/shared/event.service'
+import { ToastrService } from './common/toastr.service'
 
 @NgModule({
   imports: [
@@ -17,6 +19,7 @@ import { NavBarComponent } from './nav/navbar.component';
     EventsThumbnailComponent,
     NavBarComponent
   ],
+  providers: [EventService, ToastrService],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
--- src/app/events/events-thumbnail.component.ts
+++ src/app/events/events-thumbnail.component.ts
@@ -4,17 +4,37 @@ import { Component, Input } from '@angular/core'
 	selector: 'event-thumbnail',
 	template: `
 	<div class="well hoverwell thumbnail">
-		<h2>{{event.name}}</h2>
-		<div>Date: {{event.date}}</div>
-		<div>Time: {{event.time}}</div>
-		<div>Price: \${{event.price}}</div>
-		<div>
-			<span>Location: {{event.location.address}}</span>
-			<span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
+		<h2>{{event?.name}}</h2>
+		<div>Date: {{event?.date}}</div>
+		<div [ngClass] ="getStartTimeClass()" [ngSwitch]="event?.time">Time: {{event?.time}}
+			<span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+			<span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+			<span *ngSwitchDefault>(Normal Start)</span>
 		</div>
+		<div>Price: \${{event?.price}}</div>
+		<div *ngIf="event?.location">
+			<span>Location: {{event?.location?.address}}</span>
+			<span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
+		</div>
+		<div *ngIf="event?.onlineUrl">Online URL: {{event?.onlineUrl}}</div>
 	</div>
-	`
+	`,
+	styles: [`
+		.blue {color: #0000ff;}
+		.red {color: #ff0000;}
+		.bold {font-weight: bold;}
+		.green {color: #003300 !important;}
+		.thumbnail {min-height: 210px;}
+		`]
 })
 export class EventsThumbnailComponent {
 	@Input() event: any
+	getStartTimeClass(){
+	if (this.event && this.event.time === '8:00 am')
+		return 'green bold'
+	else if (this.event && this.event.time === '10:00 am')
+		return 'red'
+	return 'blue'
+	}
+
 }
